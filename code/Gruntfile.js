e0a01744441962046eac9a190ad59358484a1ed1
Raffaele Sgarro <raffaelesgarro@gmail.com>
2015-03-08 23:03:41 +0100

    Generate static pages

--- code/Gruntfile.js
+++ code/Gruntfile.js
@@ -4,20 +4,37 @@ module.exports = function(grunt){
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    clean: ['dist'],
+    clean: ['dist/'],
     assemble: {
-      options: {},
-      pages: {
+      options: {
+        layout: 'static/layout/layout.hbs'
+      },
+      ita: {
         expand: true,
         cwd: 'static',
-        src: '**/*.hbs',
+        src: 'ita/*.hbs',
+        dest: 'dist'
+      },
+      eng: {
+        expand: true,
+        cwd: 'static',
+        src: 'eng/*.hbs',
+        dest: 'dist/'
+      }
+    },
+    copy: {
+      assets: {
+        expand: true,
+        cwd: 'static',
+        src: ['images/**', 'style/**'],
         dest: 'dist/'
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('assemble');
 
-  grunt.registerTask('default', ['clean', 'assemble']);
+  grunt.registerTask('default', ['clean', 'assemble', 'copy']);
 };
